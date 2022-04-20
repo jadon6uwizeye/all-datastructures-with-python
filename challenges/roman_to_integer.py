fb55e422f59c2a012148582fdf0802e85885b4aa
UWIZEYE Jean de DIeu <jeandedieuuwizeye6@gmail.com>
2022-04-20 18:20:43 +0200

    some changes testing

---
+++ challenges/roman_to_integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def roman_cnversions(c):
+        if c == "I":
+            return 1
+        elif c== "V" :
+            return 5
+        elif c == "X" :
+            return 10
+        elif c == "L":
+            return 50
+        elif c == "c":
+            return 100
+        elif c == "D":
+            return 500
+        elif c == "M":
+            return 10000
+        else:
+            raise Exception("unknown ROman literal")
+            
+    def romanToInt(self, s):
+        value = 0
+        for character in s:
+            value = value + roman_cnversions(characer)
+            
+        return value
