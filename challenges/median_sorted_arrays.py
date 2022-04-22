cdd64a20f507d5b5c5f48c802b7f42d6c4a048b5
UWIZEYE Jean de DIeu <jeandedieuuwizeye6@gmail.com>
2022-04-22 15:16:36 +0200

    corrected finding median of two sorted arrays

--- challenges/median_sorted_arrays.py
+++ challenges/median_sorted_arrays.py
@@ -1,24 +1,48 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1, nums2):
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         nums = []
-        for  num1 in nums1:
-            if nums2[0] < num1:
-                nums = nums + nums2
-                
-            nums = nums + [num1]
-                    
-        if (nums1[len(nums1)-1] < nums2[0]):
-            nums = nums + nums2
-                
+        i=0
+        j=0
+        
+        if len(nums1) == 0 or len(nums2) == 0 :
+                nums = nums1 + nums2
+        else:
+            while True:
+                if nums1[i] < nums2[j]:
+                    nums.append(nums1[i])
+                    i +=1
+                elif nums2[j] < nums1[i]:
+                    nums.append(nums2[j])
+                    j+=1
+
+                else:
+                    nums.append(nums1[i])
+                    nums.append(nums2[j])
+                    i+=1
+                    j+=1
+
+                if i==len(nums1):
+                    nums+=nums2[j:]
+                    break
+
+                if j == len(nums2):
+                    nums+=nums1[i:]
+                    break
+
+
         nums_length = len(nums)
         if( nums_length % 2 != 0):
             index = (nums_length//2)
-            print(index)
+            print(nums)
             return nums[index]
         else:
             index1 = (nums_length//2)-1
             index2 = (nums_length//2)
-            return (nums[index1]+nums[index2])/2
+            print(nums)
+            try:
+                return (nums[index1]+nums[index2])/2
+            except IndexError:
+                return 1
 
 s = Solution()
 print(s.findMedianSortedArrays([0,1,2,3],[4,5]))
