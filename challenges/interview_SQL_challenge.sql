2d584e1f331bff7bb2e84aaa5f5920af55f92269
UWIZEYE Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2022-07-12 15:47:38 +0200

    finish SQL challenge

--- challenges/interview_SQL_challenge.sql
+++ challenges/interview_SQL_challenge.sql
@@ -5,4 +5,15 @@ SELECT avg(price) as `average_price` FROM Products;
 
 SELECT count(*) as `number of Products` FROM Products where price=18;
 
-SELECT * From orders WHERE `OrderDate` BETWEEN(1996-08-01,1996-09-06);p gyujfsda
+SELECT * From orders WHERE `OrderDate` BETWEEN(1996-08-01,1996-09-06);
+
+SELECT * FROM customers JOIN orders ON customers.customerId = orders.customerId;
+
+SELECT Customers.CustomerName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM (Orders
+INNER JOIN Customers ON Orders.customerID = customers.CustomerID)
+GROUP BY CustomerName
+HAVING COUNT(Orders.OrderID) > 3;
+SELECT CustomerName FROM Customers GROUP BY city HAVING COUNT(*) > 3;
+
+select * FROM Customers where City IN (select City from Customers GROUP BY City having COUNT(City) > 1) order by city DESC;
