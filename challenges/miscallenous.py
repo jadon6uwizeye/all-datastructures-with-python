66dfea48f67608b414c838107b7309b90f10f3c3
UWIZEYE Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2022-07-20 09:34:12 +0200

    start on miscallenous file

---
+++ challenges/miscallenous.py
@@ -0,0 +1,21 @@
+import math
+max = 4
+ar = [1,2,3,4,5,6,7]
+a = len(ar)
+print(math.ceil(a/4))
+
+ar1 = []
+ar2 = [] 
+ar3 = []
+
+for num in ar:
+    if len(ar1) == len(ar2) == len(ar3):
+        ar1.append(num)
+    elif len(ar2) == len(ar3):
+        ar2.append(num)
+    else:
+        ar3.append(num)
+        
+print(ar1)
+print(ar2)
+print(ar3)
