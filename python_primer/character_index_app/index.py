c4741db095eadbb6e0ae7c7f242bff938c917b4a
Uwizeye Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2023-01-16 17:52:14 +0200

    finish character index on books application

--- python_primer/character_index_app/index.py
+++ python_primer/character_index_app/index.py
@@ -9,21 +9,25 @@ data = data.lower()
 data_list = data.split()
 
 CHARACTERS = [
-    "Kurtz", "manager", "Rusian", "pilgrims", "cannibals", "aunt", "brickmaker", "Accountant", "helsman", "mistress", "intendend"
+    "Kurtz", 
+    "manager", 
+     "Rusian", 
+     "pilgrims",
+       "cannibals", "aunt", "brickmaker", "Accountant", "helsman", "mistress", "intendend"
     ]
 
 def paginate(data, page_length):
-    pages = []
-    page = []
-    data = data.split()
-    for i in range(len(data)):
-        page.append(data[i])
-        if (i+1) % page_length == 0:
-            pages.append(page)
-            page = []
-    if page:
-        pages.append(page)
-    return pages
+    # pages = []
+    # page = []
+    # data = data.split()
+    # for i in range(len(data)):
+    #     page.append(data[i])
+    #     if (i+1) % page_length == 0:
+    #         pages.append(page)
+    #         page = []
+    # if page:
+    #     pages.append(page)
+    # return pages
 
     # or we can use this algorithm
     words = data.split()
@@ -36,6 +40,9 @@ def paginate(data, page_length):
     return pages
 
 def get_names(data,characters=CHARACTERS):
+    '''
+    This function takes a string of text and a list of characters and returns a dictionary of characters and their names
+    '''
     names = defaultdict(list)
     words = data.split()
     for character in characters:
@@ -44,6 +51,24 @@ def get_names(data,characters=CHARACTERS):
                 names[character].append(word)
     return names
 
-# main
-names = get_names(data)
-print(names)
+def make_index(names,pages):
+    '''
+    This function takes a dictionary of characters and their names and a list of pages and returns a dictionary of characters and their index
+    '''
+    index = defaultdict(list)
+    for character, names in names.items():
+        for name in names:
+            for page_number, page in pages.items():
+                if name in page:
+                    index[character].append(page_number)
+    return index
+
+def print_index(index):
+    for name, pages in sorted(index.items()):
+        print("{:20}".format(name), end="", flush=True)
+        for page in pages:
+            print(f", {page}", end="", flush=True)
+        print()
+
+
+print_index(make_index(names=get_names(data),pages=paginate(data,10000)))
