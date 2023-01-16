9b9d823dc7d7786ab4d149d29ac084b00962dc3e
Uwizeye Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2023-01-16 14:42:23 +0200

    fix on Counter while reading file

--- python_primer/counter.py
+++ python_primer/counter.py
@@ -1,15 +1,16 @@
 from collections import Counter
 
 # open file sample.txt
-with open('sample.txt', 'r') as f :
-    # read the file
-    data = f.read()
-    # clean data by removing unnecessary panctuations
-    data = data.replace(',', '').replace('.', '').replace(';', '').replace('?', '').replace('!', '').replace(':', '')
-    # convert data to list
-    data_list = data.lower().split()
-    # create a counter
-    counter = Counter(data_list)
-    print(counter)
-    # print the most common words
-    print(counter.most_common(5))
+f = open('/home/jadon6/Documents/all-datastructures-with-python/python_primer/sample.txt','r')
+
+# read the file
+data = f.read()
+# clean data by removing unnecessary panctuations
+data = data.replace(',', '').replace('.', '').replace(';', '').replace('?', '').replace('!', '').replace(':', '')
+# convert data to list
+data_list = data.lower().split()
+# create a counter
+counter = Counter(data_list)
+print(counter)
+# print the most common words
+print(counter.most_common(5))
